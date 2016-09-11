81d190d51a56b219e062821e7c74d3e58509e3ad
Luis <lrivas2@gmail.com>
2016-09-11 15:43:05 -0700

    Combined with type casting lecture.

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Variables
 {
     class Program
@@ -11,7 +6,17 @@ namespace Variables
         static void Main(string[] args)
         {
             Console.WriteLine("{0}  {1}",byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0}  {1}", float.MinValue, float.MaxValue);
+            Console.WriteLine("{0}  {1}", int.MinValue, int.MaxValue);
+            Double i = Double.MaxValue;
+            Console.WriteLine(i);
+            byte b = (byte)i;
+            Console.WriteLine(b);
+            string s = "1.5";
+            decimal j = Convert.ToDecimal(s);
+            Console.WriteLine(j);
+
+
+
         }
     }
 }
