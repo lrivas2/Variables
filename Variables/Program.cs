8a38df6e60132d20dd0502dbed436c103e1832b1
Luis <lrivas2@gmail.com>
2016-09-10 23:44:12 -0700

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}  {1}",byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0}  {1}", float.MinValue, float.MaxValue);
+        }
+    }
+}
